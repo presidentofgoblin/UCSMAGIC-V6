ecb4faf4cf274724f4e3ec059349604a22cdbe97
Magich <75262956+presidentofgoblin@users.noreply.github.com>
2021-02-28 21:46:44 +0700

    Add files via upload

---
+++ Ultrapowa Clash Server/Logic/StreamEntry/ChatStreamEntry.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using UCS.Helpers;
+
+namespace UCS.Logic.StreamEntry
+{
+    internal class ChatStreamEntry : StreamEntry
+    {
+        string m_vMessage;
+
+        public override byte[] Encode()
+        {
+            List<byte> data = new List<byte>();
+            data.AddRange(base.Encode());
+            data.AddString(m_vMessage);
+            return data.ToArray();
+        }
+
+        public string GetMessage() => m_vMessage;
+
+        public override int GetStreamEntryType() => 2;
+
+        public override void Load(JObject jsonObject)
+        {
+            m_vMessage = jsonObject["message"].ToObject<string>();
+        }
+
+        public override JObject Save(JObject jsonObject)
+        {
+            jsonObject.Add("message", m_vMessage);
+            return jsonObject;
+        }
+
+        public void SetMessage(string message) => m_vMessage = message;
+    }
+}
